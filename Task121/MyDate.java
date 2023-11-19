b246c2d8b9f8b4c6e97130c817b28a76dc02900f
ejclayson <109399030+ejclayson@users.noreply.github.com>
2023-11-19 22:32:11 +0800

    Added Task Sheet 1-2-2, DateTask.java, and MyDate.java.

---
+++ Task121/MyDate.java
@@ -0,0 +1,14 @@
+package Task121;
+import Task121.DateTask;
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask();
+        DateTask date2 = new DateTask();
+        date1.setDate(1,1,1978);
+        date2.setDate(21,9, 1984);
+        System.out.println(date1);
+        System.out.println(date2);
+
+       // display leapyears
+    }
+}
